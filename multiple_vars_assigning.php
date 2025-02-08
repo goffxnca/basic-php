72e17b4ab6c52f882cff7992da826c325ad63c7e
Goff Phatt <goffxnca@gmail.com>
2025-02-08 12:30:30 +0700

    multi vars assign

---
+++ multiple_vars_assigning.php
@@ -0,0 +1,7 @@
+<?php
+$john_age = $jeff_age = $andrew_age = 30;
+echo "John age: $john_age <br>";
+echo "John age: $jeff_age <br>";
+echo "John age: $andrew_age <br>";
+
+// All variables can be assigned same value in 1 line
