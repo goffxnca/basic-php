365ff6065fc243bec9f87efe8367b0904b1b66cb
Goff Phatt <goffxnca@gmail.com>
2025-02-08 13:16:09 +0700

    learn var dump

---
+++ var_dump.php
@@ -0,0 +1,52 @@
+<?php
+$name = "john";
+echo var_dump($name);
+echo "<br>";
+
+$age = 50;
+echo var_dump($age);
+echo "<br>";
+
+echo var_dump("here is the string");
+echo "<br>";
+
+echo var_dump(183.48322);
+echo "<br>";
+
+$is_true = true;
+echo var_dump($is_true);
+echo "<br>";
+
+$fruits = ["banana", "apple"];
+echo var_dump($fruits);
+echo "<br>";
+
+$person1 = ["name" => "john", "lastname" => "doe"];
+$person2 = ["name" => "goff", "lastname" => "deh"];
+var_dump($person1);
+echo "<br>";
+
+echo json_encode($person1);
+echo "<br>";
+
+$people = [$person1, $person2];
+var_dump($people);
+echo "<br>";
+
+
+
+var_dump(5);
+echo "<br>";
+var_dump("five");
+echo "<br>";
+var_dump(2.532);
+echo "<br>";
+var_dump(true);
+echo "<br>";
+var_dump(["banana", "mango"]);
+echo "<br>";
+var_dump(["name" => "john", "lastname" => "doe"]);
+echo "<br>";
+var_dump(null);
+var_dump(NuLl);
+var_dump(NULL);
