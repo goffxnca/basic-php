3b778ac5fc7960f74dbb0e112511b62b44ef5c61
Goff Phatt <goffxnca@gmail.com>
2025-02-08 13:23:30 +0700

    learn variables

---
+++ variables.php
@@ -0,0 +1,26 @@
+<?php
+$name = "john";
+$age = 50;
+
+
+echo $name . " is " . $age . " years old<br>";
+
+$total_number = 45;
+echo "Total number: ", $total_number, "<br>";
+
+
+$_title = "This is title<br>";
+echo $_title;
+
+# Variable names are case-sensitive
+$car = "Toyota";
+$CAR = "Honda";
+
+
+echo "My car brand is: $car <br>";
+echo "And my another car brand is" . $CAR . "!<br>";
+
+$price = 500;
+$discount = 50;
+$total_price = $price - $discount;
+echo "Total price: $total_price";
