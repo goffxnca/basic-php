247fc47deb55483d4311baa661ab5c5d8538a554
Goff Phatt <goffxnca@gmail.com>
2025-02-08 12:27:25 +0700

    learn commenting

---
+++ comments.php
@@ -0,0 +1,31 @@
+<?php
+echo "PHP Comments";
+// Single line comments with double slashes
+# Single line comment with hash
+/*
+This is multi-lines comment
+A Fox
+Jump
+Over
+The
+Box
+*/
+
+// Show welcome message
+echo "Welcome";
+
+echo "I love PHP"; // Also so message
+
+echo "I love coding"; #Show another message
+
+// echo "Brown Fox"
+
+
+/*
+echo "John"
+echo "Doe"
+*/
+
+
+$result = 5 /* It's a math */ + 5;
+echo $result;
